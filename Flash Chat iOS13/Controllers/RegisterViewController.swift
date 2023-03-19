aced96c2041c880f7e4f972b9eed8287e07563ed
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-03-19 13:44:40 +0300

    added Pod Firebase + login func2

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(alert, animated: true)
+                    print(e.localizedDescription)
+                } else {
+                    // Navigate to Chat View Controller
+                    self.performSegue(withIdentifier: Segues.toChatFromRegister.rawValue, sender: self
+                    )
+                }
+            }
+        }
     }
     
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
+import CLTypingLabel
+
+enum Segues: String, CaseIterable {
+    case registerVC = "goToRegisterVC"
+    case loginVC = "goToLoginVC"
+    case toChatFromRegister = "registerToChatVC"
+    case toChatFromLogin = "loginToChatVC"
+}
+
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
+    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //animateTitleLabel()
        
+        titleLabel.text = "⚡️FlashChat"
     }
     
-
+    func animateTitleLabel() {
+        let titleStr = "⚡️FlashChat"
+        titleLabel.text = ""
+        var charIndex: Double = 0
+        titleStr.forEach { char in
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(char)
+            }
+            charIndex += 1
+        }
+    }
+    
 }
